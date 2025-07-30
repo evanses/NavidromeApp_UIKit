f7f0bf7d0f0f7e8ae78ecc5774c5beea896234b8
eva <vacumase@gmail.com>
2025-07-30 11:43:01 +0500

    add confirmation when logout

--- Navidrome/SettingsView/SettingsViewController.swift
+++ Navidrome/SettingsView/SettingsViewController.swift
@@ -69,6 +69,95 @@ class SettingsViewController: UIViewController {
         return label
     }()
     
+    private lazy var srvView: UIView = {
+        let contentview = UIView()
+        
+        contentview.translatesAutoresizingMaskIntoConstraints = false
+        contentview.backgroundColor = .white
+        contentview.layer.cornerRadius = 10
+        contentview.clipsToBounds = false
+        
+        contentview.layer.shadowColor = UIColor.black.cgColor
+        contentview.layer.shadowOpacity = 0.1
+        contentview.layer.shadowOffset = CGSize(width: 0, height: -2)
+        contentview.layer.shadowRadius = 4
+
+        return contentview
+    }()
+    
+    private lazy var srvVersionTitleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 18)
+        label.text = "Версия"
+        return label
+    }()
+    
+    private lazy var srvVersionLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 18)
+        label.text = ""
+        return label
+    }()
+    
+    private lazy var secondSeparator: UIView = {
+        let contentview = UIView()
+        
+        contentview.translatesAutoresizingMaskIntoConstraints = false
+        contentview.backgroundColor = .myBackground
+        return contentview
+    }()
+
+    private lazy var srvTypeTitleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 18)
+        label.text = "Сервер"
+        return label
+    }()
+
+
+    private lazy var srvTypeLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 18)
+        label.text = ""
+        return label
+    }()
+    
+    private lazy var thirdSeparator: UIView = {
+        let contentview = UIView()
+        
+        contentview.translatesAutoresizingMaskIntoConstraints = false
+        contentview.backgroundColor = .myBackground
+        return contentview
+    }()
+
+    private lazy var srvVersionMainTitleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 18)
+        label.text = "Версия сервера"
+        return label
+    }()
+
+
+    private lazy var srvVersionMainLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 18)
+        label.text = ""
+        return label
+    }()
+
+    
     private lazy var logoutView: UIView = {
         let contentview = UIView()
         
@@ -94,6 +183,31 @@ class SettingsViewController: UIViewController {
         label.isUserInteractionEnabled = true
         return label
     }()
+    
+    // MARK: AlertViews
+    
+    private lazy var confirmAlertView: UIAlertController = {
+        let newAlertController = UIAlertController(title: "Вы уверены что хотите выйти?", message: nil, preferredStyle: .alert)
+        
+        newAlertController.addAction(UIAlertAction(
+            title: "Выйти",
+            style: .destructive,
+            handler: { action in
+                self.logout()
+            })
+        )
+        
+        newAlertController.addAction(UIAlertAction(
+            title: "Отмена",
+            style: .cancel,
+            handler: { action in
+                
+            })
+        )
+        
+        return newAlertController
+    }()
+
     
     // MARK: - Lifecycle
     
@@ -128,6 +242,16 @@ class SettingsViewController: UIViewController {
         urlLoginView.addSubview(loginTitleLabel)
         urlLoginView.addSubview(loginLabel)
         
+        view.addSubview(srvView)
+        srvView.addSubview(srvVersionTitleLabel)
+        srvView.addSubview(srvVersionLabel)
+        srvView.addSubview(secondSeparator)
+        srvView.addSubview(srvTypeTitleLabel)
+        srvView.addSubview(srvTypeLabel)
+        srvView.addSubview(thirdSeparator)
+        srvView.addSubview(srvVersionMainTitleLabel)
+        srvView.addSubview(srvVersionMainLabel)
+        
         view.addSubview(logoutView)
         logoutView.addSubview(logoutLabel)
     }
@@ -160,7 +284,41 @@ class SettingsViewController: UIViewController {
             
             urlLoginView.bottomAnchor.constraint(equalTo: loginLabel.bottomAnchor, constant: 8.0),
             
-            logoutView.topAnchor.constraint(equalTo: urlLoginView.bottomAnchor, constant: 8.0),
+            srvView.topAnchor.constraint(equalTo: urlLoginView.bottomAnchor, constant: 8.0),
+            srvView.leadingAnchor.constraint(equalTo: safeAreaGuide.leadingAnchor, constant: leadingTrailingConstant),
+            srvView.trailingAnchor.constraint(equalTo: safeAreaGuide.trailingAnchor, constant: -leadingTrailingConstant),
+            
+            srvVersionTitleLabel.topAnchor.constraint(equalTo: srvView.topAnchor, constant: 8.0),
+            srvVersionTitleLabel.leadingAnchor.constraint(equalTo: srvView.leadingAnchor, constant: 8.0),
+            
+            srvVersionLabel.topAnchor.constraint(equalTo: srvVersionTitleLabel.bottomAnchor, constant: 4.0),
+            srvVersionLabel.leadingAnchor.constraint(equalTo: srvView.leadingAnchor, constant: 8.0),
+            
+            secondSeparator.topAnchor.constraint(equalTo: srvVersionLabel.bottomAnchor, constant: 4.0),
+            secondSeparator.leadingAnchor.constraint(equalTo: srvView.leadingAnchor, constant: 8.0),
+            secondSeparator.trailingAnchor.constraint(equalTo: srvView.trailingAnchor, constant: -8.0),
+            secondSeparator.heightAnchor.constraint(equalToConstant: 1.0),
+            
+            srvTypeTitleLabel.topAnchor.constraint(equalTo: secondSeparator.bottomAnchor, constant: 4.0),
+            srvTypeTitleLabel.leadingAnchor.constraint(equalTo: srvView.leadingAnchor, constant: 8.0),
+            
+            srvTypeLabel.topAnchor.constraint(equalTo: srvTypeTitleLabel.bottomAnchor, constant: 4.0),
+            srvTypeLabel.leadingAnchor.constraint(equalTo: srvView.leadingAnchor, constant: 8.0),
+            
+            thirdSeparator.topAnchor.constraint(equalTo: srvTypeLabel.bottomAnchor, constant: 4.0),
+            thirdSeparator.leadingAnchor.constraint(equalTo: srvView.leadingAnchor, constant: 8.0),
+            thirdSeparator.trailingAnchor.constraint(equalTo: srvView.trailingAnchor, constant: -8.0),
+            thirdSeparator.heightAnchor.constraint(equalToConstant: 1.0),
+
+            srvVersionMainTitleLabel.topAnchor.constraint(equalTo: thirdSeparator.bottomAnchor, constant: 4.0),
+            srvVersionMainTitleLabel.leadingAnchor.constraint(equalTo: srvView.leadingAnchor, constant: 8.0),
+            
+            srvVersionMainLabel.topAnchor.constraint(equalTo: srvVersionMainTitleLabel.bottomAnchor, constant: 4.0),
+            srvVersionMainLabel.leadingAnchor.constraint(equalTo: srvView.leadingAnchor, constant: 8.0),
+            
+            srvView.bottomAnchor.constraint(equalTo: srvVersionMainLabel.bottomAnchor, constant: 8.0),
+            
+            logoutView.topAnchor.constraint(equalTo: srvView.bottomAnchor, constant: 8.0),
             logoutView.leadingAnchor.constraint(equalTo: safeAreaGuide.leadingAnchor, constant: leadingTrailingConstant),
             logoutView.trailingAnchor.constraint(equalTo: safeAreaGuide.trailingAnchor, constant: -leadingTrailingConstant),
             
@@ -181,11 +339,19 @@ class SettingsViewController: UIViewController {
     private func setupView() {
         hostLabel.text = viewModel.host
         loginLabel.text = viewModel.login
+        
+        srvVersionLabel.text = viewModel.version
+        srvTypeLabel.text = viewModel.type
+        srvVersionMainLabel.text = viewModel.serverVersion
     }
     
     // MARK: Actions
     
     @objc private func logoutTapped() {
+        self.present(confirmAlertView, animated: true)
+    }
+    
+    @objc private func logout() {
         viewModel.logout()
     }
 }
--- Navidrome/SettingsView/SettingsViewModel.swift
+++ Navidrome/SettingsView/SettingsViewModel.swift
@@ -1,6 +1,12 @@
+import Foundation
+
 final class SettingsViewModel {
     var host: String = ""
     var login: String = ""
+    
+    var version: String = ""
+    var type: String = ""
+    var serverVersion: String = ""
     
     init() {
         guard let apiHost = KeychainManager.shared.getHost(),
@@ -10,6 +16,23 @@ final class SettingsViewModel {
             }
         self.host = apiHost
         self.login = login
+        
+        NavidromeApiManager.shared.loginCheck(
+            host: apiHost,
+            login: login,
+            password: pass) { result in
+                switch result {
+                case .success(let res):
+                    DispatchQueue.main.async {
+                        self.version = res.version
+                        self.type = res.type
+                        self.serverVersion = res.serverVersion
+                    }
+                case .failure(let error):
+                    print("error while loginCheck in SettingViewModel")
+                }
+            }
+
     }
     
     func logout() {
